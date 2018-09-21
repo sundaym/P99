5687558ce0a9f63313b8f2a4347a9440759b1e26
sundaym <sunday2011s@gmail.com>
2018-09-21 10:11:11 +0800

    P09

--- Scheme/1-Prolog Lists/1.09.rkt
+++ Scheme/1-Prolog Lists/1.09.rkt
@@ -3,8 +3,14 @@
 ;--------------------------------------------------------------------
 ;P09 (**)Pack consecutive duplicates of list elements into sublists.
 ;--------------------------------------------------------------------
-(define (pack ls)
-  (group ls))
-
-(define (group ls)
-  ())
+(define pack 
+   (lambda (xs) 
+     (if (null? xs) '() 
+       (let loop ((rest (cdr xs)) 
+                  (pkg (list (car xs)))) 
+         (cond ((null? rest) 
+                (list pkg)) 
+               ((eq? (car pkg) (car rest)) 
+                (loop (cdr rest) (cons (car rest) pkg))) 
+               (else 
+                 (cons pkg (loop (cdr rest) (list (car rest))))))))))
