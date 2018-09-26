d42d984fe55f876f2cf36c9a0ae1af1dac688cf0
sundaym <sunday2011s@gmail.com>
2018-09-26 15:31:38 +0800

    P09

--- Scheme/1-Prolog Lists/1.09.rkt
+++ Scheme/1-Prolog Lists/1.09.rkt
@@ -3,12 +3,11 @@
 ;--------------------------------------------------------------------
 (define pack 
    (lambda (xs) 
-     (if (null? xs) '() 
-       (let loop ((rest (cdr xs)) 
-                  (pkg (list (car xs)))) 
-         (cond ((null? rest) 
-                (list pkg)) 
-               ((eq? (car pkg) (car rest)) 
-                (loop (cdr rest) (cons (car rest) pkg))) 
-               (else 
-                 (cons pkg (loop (cdr rest) (list (car rest))))))))))
+     (if (null? xs)
+         '()
+         (let loop ((rest (cdr xs))
+                    (pkg (list (car xs))))
+           (cond ((null? rest) (list pkg))
+                 ((eq? (car pkg) (car rest)) (loop (cdr rest) (cons (car rest) pkg)))
+                 (else
+                  (cons pkg (loop (cdr rest) (list (car rest))))))))))
