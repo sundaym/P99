e711ce045427467098e0312c6b6a004627908067
sundaym <sunday2011s@gmail.com>
2024-11-01 13:39:08 +0800

    Update P1.java

--- Java/P1.java
+++ Java/P1.java
@@ -1,1 +1,9 @@
+import java.util.List;
 
+public class P1 {
+    // 获取最后一个元素
+    public static Object getTheLastElement(List<Object> list) {
+        int size = list.size();
+        return list.get(size - 1);
+    }
+}
